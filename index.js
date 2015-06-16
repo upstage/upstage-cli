cb2fadcdbbf74e655a995b7b230140846dbf7cdc
Brian Woodward <brian.woodward@gmail.com>
2015-06-15 21:34:40 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * ghp <https://github.com/doowb/ghp>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
